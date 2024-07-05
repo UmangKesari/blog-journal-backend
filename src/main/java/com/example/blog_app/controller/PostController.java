93084ef63042a3f9b82a9a0c75c5ca4951ad2993
UmangKesari <umang.kesari17@gmail.com>
2024-07-05 11:04:44 +0530

    Added file upload/download controller handler

--- src/main/java/com/example/blog_app/controller/PostController.java
+++ src/main/java/com/example/blog_app/controller/PostController.java
@@ -4,12 +4,20 @@ import com.example.blog_app.config.AppConstants;
 import com.example.blog_app.dto.APIResponse;
 import com.example.blog_app.dto.PostDTO;
 import com.example.blog_app.dto.PostPaginatedResponse;
+import com.example.blog_app.services.FileService;
 import com.example.blog_app.services.PostService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -18,6 +26,12 @@ public class PostController {
 
     @Autowired
     private PostService postService;
+
+    @Autowired
+    private FileService fileService;
+
+    @Value("${project.image}")
+    private String path;
 
     @PostMapping("/user/{userId}/category/{categoryId}/posts")
     public ResponseEntity<PostDTO> createPost(@RequestBody PostDTO postDTO,
@@ -44,7 +58,7 @@ public class PostController {
 
     @GetMapping("/posts")
     public ResponseEntity<PostPaginatedResponse> getAllPost(
-            @RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER,required = false) Integer pageNumber,
+            @RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
             @RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
             @RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
             @RequestParam(value = "sortDirection", defaultValue = AppConstants.SORT_DIR, required = false) String sortDirection
@@ -72,6 +86,34 @@ public class PostController {
         PostDTO updatedPost = postService.updatePost(postDTO, postId);
 
         return new ResponseEntity<>(updatedPost, HttpStatus.OK);
+    }
+
+    @PostMapping("/post/image/upload/{postId}")
+    public ResponseEntity<PostDTO> uploadPostImage(
+            @PathVariable Integer postId,
+            @RequestParam("image") MultipartFile image
+    ) throws IOException {
+
+        PostDTO postDTO = postService.getPostById(postId);
+
+        String fileName = fileService.uploadImage(path, image);
+
+        //update in DB
+        postDTO.setImageName(fileName);
+        PostDTO updatedPost = postService.updatePost(postDTO, postId);
+
+        return new ResponseEntity<>(updatedPost, HttpStatus.OK);
+
+    }
+
+    @GetMapping(value = "/post/image/{imageName}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public void downloadImage(@PathVariable("imageName") String imageName,
+                              HttpServletResponse response) throws IOException {
+
+        InputStream resource = fileService.getResource(path, imageName);
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource, response.getOutputStream());
+
     }
 
 }
